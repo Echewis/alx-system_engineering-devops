18b59cddf7b6b8a08df800e4618bda5cc8facebd
WisDomz <houseofwem1@gmail.com>
2023-11-28 17:03:05 +0100

    same expression methodz

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,4 @@
 #!/usr/bin/env ruby
+# This is a ruby script that accepts one argument of REGEX
+# and passes it to the regular expression matching method
 puts ARGV[0].scan(/School/).join
--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # This script will accept one argument and
 # pass it to a regular expression matchin method
-puts ARGV[0].scan(/hbt{1,4}n/).join
+puts ARGV[0].scan(/hbt+n/)
